3a11f215ce11a39251e7950218cfa70a24eb7901
ryo.tezuka <tezuka0226@gmail.com>
2014-08-12 20:57:09 +0900

    Add! placeholder

--- app.js
+++ app.js
@@ -14,6 +14,22 @@ app.get('/about', function(req, res) {
     res.send('about this page')
 });
 
+// マジでRailsみたいな感じでとれる、コロンの部分をプレースホルダという
+// はてなをつけるとオプショナル！Swiftやんけ！
+app.get('/users/:name?', function(req, res) {
+    if (req.params.name) {
+        res.send('Hello ' + req.params.name);
+    } else {
+        res.send('Hello Guest!');
+    }
+});
+
+// routerには正規表現も使えるよ
+app.get('/items/:id([0-9]+)', function(req, res) {
+    res.send('item no: ' + req.params.id);
+});
+
+
 // 3000番のポートで待ち受け！
 app.listen(3000);
 console.log('server starting...');
